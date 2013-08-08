3e00349ca0475c9551e298ba6da8758db441ca83
puppetMaster3 <vic.cvc@gmx.com>
2013-08-08 09:17:35 -0700

    nyc prep

--- 3blog/aCDN/app/BlogApp.ts
+++ 3blog/aCDN/app/BlogApp.ts
@@ -1,17 +1,23 @@
 viewDir = '../aCDN/view/'
-console.log('v0.0')
+console.log('v0.1')
 
 snapper = new Snap({
     element: document.getElementById('content')
 })
 snapper.close()
 
-
 class EnterForm {
     constructor() {
         forward('enterForm', 'enterForm')
-    }
+    }//()
+}
+//frm = new EnterForm()
 
+class Posting {
+    constructor() {
+        forward('Posting', 'Posting')
+    }//()
 }
 
-frm = new EnterForm()
+pst = new Posting()
+
