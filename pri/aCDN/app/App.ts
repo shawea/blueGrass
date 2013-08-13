9e13c4b26b7f57140ccceca28ef373c0141a5ad4
Victor Cekvenich <vicmcvc (at) gmx.com or puppetMaster3 on twitter>
2013-08-13 15:40:53 -0700

    done?

--- pri/aCDN/app/App.ts
+++ pri/aCDN/app/App.ts
@@ -5,14 +5,18 @@ console.log('1')
 var navFlag = false
 var navBtn = document.querySelector('.topcoat-icon--menu-stack')
 navBtn.addEventListener('click', toggleSideNav, false)
+
+var nav = document.getElementById('slider')
+nav.addEventListener('click', toggleSideNav, false)
+
 function toggleSideNav () {
     console.log('side')
     if(!navFlag) {
         TweenLite.to('#slider',.2,{x:400})
-        TweenLite.to('#container',.2,{x:400})
+        //TweenLite.to('#container',.2,{x:400})
     } else {
         TweenLite.to('#slider',.2,{x:0})
-        TweenLite.to('#container',.2,{x:0})
+       // TweenLite.to('#container',.2,{x:0})
     }
     navFlag = !navFlag
 }
