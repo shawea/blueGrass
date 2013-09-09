37799874d7c084dd9fefc6d1e05afeec2eb7f116
Vic Cekvenich <puppetMaster3 on twitter>
2013-09-09 12:18:33 -0700

    unfucked from refactor

--- zother/pri/pCDN/app/preLoa.js
+++ zother/pri/pCDN/app/preLoa.js
@@ -2,17 +2,17 @@ document.body.style.cursor = 'wait';
 
 if ('__proto__' in {}) {
     //jq or z
-    document.write('<script src="http://scdn.primus.netdna-latest.com/latest/zepto.min.js"><\/script>');
+    document.write('<script src="http://scdn.primus.netdna-cdn.com/latest/zepto.min.js"><\/script>');
     console.log('z');
 } else {
-    document.write('<script src="http://scdn.primus.netdna-latest.com/latest/more/jquery-2.0.3.min.js"><\/script>');
+    document.write('<script src="http://scdn.primus.netdna-cdn.com/latest/more/jquery-2.0.3.min.js"><\/script>');
     console.log('jq');
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DL');
     setTimeout(function () {
-        head.js('http://scdn.primus.netdna-latest.com/latest/TweenLite.min.js', 'http://scdn.primus.netdna-latest.com/latest/more/signals.min.js', 'http://scdn.primus.netdna-latest.com/latest/fastclick.js', 'http://scdn.primus.netdna-latest.com/latest/blueGrass.js', 'http://scdn.primus.netdna-latest.com/latest/CSSPlugin.min.js', 'http://primusAPI.com/pCDN/app/Pre.js', 'http://primusAPI.com/pCDN/app/App.js');
+        head.js('http://scdn.primus.netdna-cdn.com/latest/TweenLite.min.js', 'http://scdn.primus.netdna-cdn.com/latest/more/signals.min.js', 'http://scdn.primus.netdna-cdn.com/latest/fastclick.js', 'http://scdn.primus.netdna-cdn.com/latest/blueGrass.js', 'http://scdn.primus.netdna-cdn.com/latest/CSSPlugin.min.js', 'http://primusAPI.com/pCDN/app/Pre.js', 'http://primusAPI.com/pCDN/app/App.js');
     }, 10);
 });
 //# sourceMappingURL=preLoa.js.map
