00b1fdfab276b191301138e161f813b464004f56
Vic Cekvenich <puppetMaster3 on twitter>
2013-09-25 13:48:05 -0700

    almost slider

---
+++ www/mCDN/app/main.js
@@ -0,0 +1,42 @@
+//loader
+document.body.style.cursor = 'wait';
+require([
+    '//scdn.primus.netdna-cdn.com/latest/jquery-2.1.b1.js',
+    '//scdn.primus.netdna-cdn.com/latest/fastclick.js',
+    '//scdn.primus.netdna-cdn.com/latest/TweenLite.min.js',
+    '//scdn.primus.netdna-cdn.com/latest/blueGrass.js'
+], function () {
+    require([
+        '//scdn.primus.netdna-cdn.com/latest/css/bootstrap.js',
+        '//scdn.primus.netdna-cdn.com/latest/CSSPlugin.min.js',
+        '//scdn.primus.netdna-cdn.com/latest/more/parallax.js',
+        '//scdn.primus.netdna-cdn.com/latest/cloudAPI.js',
+        '//scdn.primus.netdna-cdn.com/latest/more/transparency.min.js'
+    ], function () {
+        console.log('loaded v0.2');
+        FastClick.attach(document.body);
+
+        document.body.style.cursor = 'default';
+    });
+});
+
+// main code
+var hamBut = document.getElementById('hamburger');
+hamBut.addEventListener('click', function () {
+    toggleSideNav();
+});
+
+var navFlag;
+function toggleSideNav() {
+    console.log('slider');
+    if (!navFlag) {
+        TweenLite.to('#slider', .2, { x: 405 });
+        TweenLite.to('#kontainer', .2, { x: 405 });
+    } else {
+        TweenLite.to('#slider', .2, { x: 0 });
+        TweenLite.to('#kontainer', .2, { x: 0 });
+    }
+    navFlag = !navFlag;
+}//()
+
+//# sourceMappingURL=main.js.map
