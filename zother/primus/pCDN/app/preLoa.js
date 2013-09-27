c865fb4afb36ea2c4424c43ac339b5b6baa24239
Vic Cekvenich <puppetMaster3 on twitter>
2013-09-26 18:28:58 -0700

    sync

--- zother/primus/pCDN/app/preLoa.js
+++
@@ -1,9 +0,0 @@
-document.body.style.cursor = 'wait';
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DL');
-    setTimeout(function () {
-        head.js('http://scdn.primus.netdna-cdn.com/latestO/more/jquery-2.0.3.min.js', 'http://scdn.primus.netdna-cdn.com/latestO/TweenLite.min.js', 'http://scdn.primus.netdna-cdn.com/latestO/signals.min.js', 'http://scdn.primus.netdna-cdn.com/latestO/blueGrass.js', 'http://scdn.primus.netdna-cdn.com/latestO/CSSPlugin.min.js', 'pCDN/app/Pre.js', 'pCDN/app/App.js');
-    }, 10);
-});
-//# sourceMappingURL=preLoa.js.map
